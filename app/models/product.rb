41ed98fcc196773ff38dcfffce065ac0aac23b16
imranrrr <emranlateef786@gmail.com>
2023-09-19 17:44:23 +0500

    initial

--- app/models/product.rb
+++
@@ -1,4 +0,0 @@
-class Product < ApplicationRecord
-    has_many :sales, dependent: :destroy
-    has_many :track_inventories, dependent: :destroy
-end
--- app/models/track_inventory.rb
+++
@@ -1,3 +0,0 @@
-class TrackInventory < ApplicationRecord
-    belongs_to :product
-end
